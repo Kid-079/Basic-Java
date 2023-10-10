65177a55bf1a9c3968cce9bbbfa10c0327894c18
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2023-10-11 01:06:20 +0900

    Add files via upload

---
+++ 03 String/1/source/com/dataString/Main.java
@@ -0,0 +1,21 @@
+package com.dataString;
+
+public class Main {
+	public static void main(String[] args){
+		System.out.println("The Weather is Nice Today");
+		System.out.println("Yeah, it is cool and refreshing");
+		
+		System.out.print("I'd like to relax by the river");
+		System.out.print("It's very hot today");
+		
+		System.out.print("The Plane is going to take off in a few minutes \n");
+		System.out.print("I'm Nervous \n");
+		
+		System.out.printf("I'm glad you are coming \n");
+		System.out.printf("You look Wonderful %d \n", 123);
+
+
+	}
+}
+
+
---
+++ 03 String/2/source/com/dataString/Main.java
@@ -0,0 +1,14 @@
+package com.dataString;
+
+public class Main {
+	public static void main(String[] args){
+		System.out.println("Good " + args[0] + " Night");    // Show args Value
+		System.out.println("Good " + args[1] + " Morning");  // Show args Value
+		System.out.println("Good Evening"); 
+	}
+}
+
+
+
+// Run Program :
+// - java Main.java A B 
