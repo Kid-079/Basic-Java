e3cc881d468747876e34b2621afb8ae6813fa298
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2023-10-20 16:29:02 +0900

    Add files via upload

---
+++ 03 String/6/source/com/dataString/Main.java
@@ -0,0 +1,30 @@
+package com.dataString;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args){
+
+		String string_1 = "ABC";
+		String string_2 = "Abc";
+		String string_3 = "Abcde";
+		printAddress("String_1 --> ", string_1);
+		printAddress("String_2 --> ", string_2);
+		printAddress("String_3 --> ", string_3);
+
+		string_1 = string_1.substring(0,2);     
+		// string_1 = string_1.substring(0,3);
+		printAddress("String_1", string_1);
+
+		String string_4 = "ABCDE";
+		printAddress("string_4", string_4);
+
+		String string_5 = "ABCDE";
+		printAddress("string_5", string_5);
+	}
+
+	private static void printAddress(String name, String data){
+		int address = System.identityHashCode(data);
+		System.out.println(name + " -> " + data + "\t|| address = " + Integer.toHexString(address));
+	}
+}
