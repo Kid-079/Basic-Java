85873d4b2b110ab279527de2dd40693dfcdc9346
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2023-10-23 00:17:43 +0900

    Add files via upload

---
+++ 02 Run and Compile Program/1/source/newProgram/Data.java
@@ -0,0 +1,8 @@
+package newProgram;
+
+public class Data {
+	public static void main(String[] args){
+		System.out.println("ABCDE");
+	}
+}
+
---
+++ 02 Run and Compile Program/2/source/com/dataName/Main.java
@@ -0,0 +1,11 @@
+package com.dataName;
+
+// Keywords
+public class Main {
+
+	public static void main(String[] args){
+		System.out.println("Good Afternoon");
+		System.exit(0);
+	}
+
+}
