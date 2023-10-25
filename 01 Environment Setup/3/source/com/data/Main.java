f0fbaa90b043ca0563afd3ae7565c98964b8a889
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2023-10-25 09:38:30 +0900

    Add files via upload

---
+++ 01 Environment Setup/3/source/com/data/Main.java
@@ -0,0 +1,21 @@
+// Keywords
+package com.data;
+
+public class Main {
+	public static void dataPublic() {
+		System.out.println("Good Afternoon : Public");
+	}
+
+	public static void dataPrivate() {
+		System.out.println("Good Afternoon : Private");
+	}
+
+	static void dataDefault() {
+		System.out.println("Good Afternoon : Default");
+	}
+
+	protected static void dataProtected() {
+		System.out.println("Good Afternoon : Protected");
+	}
+}
+
