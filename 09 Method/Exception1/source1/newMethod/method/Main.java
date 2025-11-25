3b774356d4de1fbd593e87125a3d04e1489e5197
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-25 20:37:36 +0700

    Add files via upload

---
+++ 09 Method/Exception1/source1/newMethod/method/Main.java
@@ -0,0 +1,14 @@
+package newMethod.method;
+
+import java.util.Arrays;
+
+public class Main {
+	static void method() {
+		System.out.println("KEEP STRUGGLE");
+	}
+	public static void main(String[] args){
+		method();
+		method();
+		method();
+	}
+}
