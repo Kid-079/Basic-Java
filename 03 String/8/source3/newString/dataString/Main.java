a57bdb1cfc0bea03114b154236539218e6600d53
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-25 19:24:56 +0700

    Add files via upload

---
+++ 03 String/8/source3/newString/dataString/Main.java
@@ -0,0 +1,21 @@
+package newString.dataString;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args){
+		// Compare
+		String string1 = "Nervous";
+		String string2 = "Confuse";
+		System.out.println(string1.compareTo(string2));
+		System.out.println(string2.compareTo(string1));
+
+		String string3 = "STRONG";
+		String string4 = "SURVIVE";
+		System.out.println(string3.compareTo(string4));
+		System.out.println(string4.compareTo(string3));
+
+		// Range A ----> a ==> 32
+		// Range A ----> K ==> 10
+	}
+}
