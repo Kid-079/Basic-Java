78431748d7cea040d60fe1fdbac59f4208c51073
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-25 20:24:31 +0700

    Add files via upload

---
+++ 08 Array/8/source4/newArray/array/Main.java
@@ -0,0 +1,40 @@
+package com.array;
+
+import java.util.Arrays;
+import java.lang.StringBuilder;
+
+public class Main {
+	public static void main(String[] args){
+		// Create New String
+		System.out.println("-> Create New String <-");
+		StringBuilder string = new StringBuilder("Confuse");
+		printData(string);
+		System.out.println();
+
+		// Append  --> Create String on New Memory
+		System.out.println("-> Append <-");
+		string.append(" SURVIVE");
+		printData(string);
+		System.out.println();
+
+		// Concat  --> Create String in New Memory
+		System.out.println("-> Concat <-");
+		String string_a = "Nervous";
+		string_a = string_a.concat(" STRONG");
+
+		System.out.println("Data    : " + string_a);
+		System.out.println("Length  : " + string_a.length());
+		
+		int addressString = System.identityHashCode(string_a);
+		System.out.println("Address : " + Integer.toHexString(addressString));			
+	}
+
+	private static void printData(StringBuilder dataString){
+		System.out.println("Data     : " + dataString);
+		System.out.println("Length   : " + dataString.length());
+		System.out.println("Capacity : " + dataString.capacity());
+
+		int addressString = System.identityHashCode(dataString);
+		System.out.println("Address  : " + Integer.toHexString(addressString));
+	}
+}
