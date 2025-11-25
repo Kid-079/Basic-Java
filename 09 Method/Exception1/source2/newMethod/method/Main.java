ee0d5281eed7b6b2da872fa05f7a7b03ed65d6dd
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-25 20:38:48 +0700

    Add files via upload

---
+++ 09 Method/Exception1/source2/newMethod/method/Main.java
@@ -0,0 +1,15 @@
+package newMethod.method;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args){
+		method();
+		method();
+		method();
+	}
+
+	private static void method() {
+		System.out.println("KEEP SURVIVE");
+	}
+}
