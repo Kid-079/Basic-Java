808d04e3b14769198f542bc7d32216ab376e0ea1
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-25 19:25:24 +0700

    Add files via upload

---
+++ 03 String/8/source4/newString/dataString/Main.java
@@ -0,0 +1,17 @@
+package newString.dataString;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args){
+		// Equality
+		String a = "Strong";
+		String b = "strong";
+
+		if(a == b){
+			System.out.println("Match");
+		} else {
+			System.out.println("Not Match");			
+		}
+ 	}
+}
